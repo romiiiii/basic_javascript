24f8ceebf485b90b62e97b7017d8cdd92e8947f1
romi <romikhoerur@gmail.com>
2022-10-10 15:30:20 +0700

    10-10-2022

---
+++ conditionalsAndLoops_ifStatement.js
@@ -0,0 +1,12 @@
+var age = 20;
+
+if(age >= 18){
+    console.log('adult')
+}
+
+//============================================================================
+
+var jumlah = 1;
+var jm = (jumlah > 5) ? "banyak" : "dikit";
+
+console.log(jm)
---
+++ conditionalsAndLoops_ifStatement_1.js
@@ -0,0 +1,4 @@
+var jam = 15;
+var pataua = (jam >= 13) ? " PM" : " AM";
+
+console.log(jam + pataua);
