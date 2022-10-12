978c9dea77efbd88078516644e38e69cd6809b31
romi <romikhoerur@gmail.com>
2022-10-12 09:41:19 +0700

    switch

---
+++ 17.2_darkTheme.js
@@ -0,0 +1,20 @@
+var theme = 2;
+
+switch(theme){
+    case 1:
+        console.log('Light Theme')
+        break;
+    case 2:
+        console.log('Dark Theme')
+        break;
+    case 3:
+        console.log('Nocturne Theme')
+        break;
+    case 4:
+        console.log('Terminal Theme')
+        break;
+    case 5:
+        console.log('Indigo Theme')
+        break;
+}
+
