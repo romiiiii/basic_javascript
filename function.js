d5778c45e39b2f42282b85c5ff2d80d16dc9449d
romi <romikhoerur@gmail.com>
2022-10-31 15:01:12 +0700

    31-oct-2022_mengulang function

---
+++ function.js
@@ -0,0 +1,15 @@
+//Calling Function
+function namaSaya(nama){
+    console.log("Hi", nama)
+}
+
+namaSaya("Romi");
+
+//Return Function
+function myFunction(a, b){
+    return console.log(a * b);
+}
+
+myFunction(5, 6);
+
+//
